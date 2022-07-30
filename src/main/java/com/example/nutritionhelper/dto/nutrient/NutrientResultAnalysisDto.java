795031797839c9d71d0e0f4be0fa11c89899bab4
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-07-30 19:37:39 +0900

    전체 분석 코드 수정

--- src/main/java/com/example/nutritionhelper/dto/nutrient/NutrientResultAnalysisDto.java
+++ src/main/java/com/example/nutritionhelper/dto/nutrient/NutrientResultAnalysisDto.java
@@ -21,14 +21,107 @@ public class NutrientResultAnalysisDto {
     Float enoughAmount;
     Float maximumAmount;
     Float amount;
+    // 초과 여부
+    int type;
 
-    public NutrientResultAnalysisDto(UserGroupNutrient userGroupNutrient, NutrientAnalysisDto nutrientAnalysisDto){
+    public NutrientResultAnalysisDto(UserGroupNutrient userGroupNutrient
+            , NutrientAnalysisDto nutrientAnalysisDto
+            , int type){
         this.nutrient = userGroupNutrient.getNutrient();
         this.averageAmount = userGroupNutrient.getAverageAmount();
         this.recommendAmount = userGroupNutrient.getRecommendAmount();
         this.enoughAmount = userGroupNutrient.getEnoughAmount();
         this.maximumAmount = userGroupNutrient.getMaximumAmount();
         this.amount = nutrientAnalysisDto.getAmount();
+        this.type = type;
+    }
+    public NutrientResultAnalysisDto(UserGroupNutrient userGroupNutrient
+            , NutrientAnalysisDto nutrientAnalysisDto){
+        this.nutrient = userGroupNutrient.getNutrient();
+        this.averageAmount = userGroupNutrient.getAverageAmount();
+        this.recommendAmount = userGroupNutrient.getRecommendAmount();
+        this.enoughAmount = userGroupNutrient.getEnoughAmount();
+        this.maximumAmount = userGroupNutrient.getMaximumAmount();
+        this.amount = nutrientAnalysisDto.getAmount();
+
+        int n = 0;
+        // 1. average만 존재하는 경우
+        // ENG
+        if(userGroupNutrient.getAverageAmount()!=null&&userGroupNutrient.getRecommendAmount()==null&&userGroupNutrient.getEnoughAmount()==null
+                &&userGroupNutrient.getMaximumAmount()==null){
+            n=1;
+        }
+        // 2. average, recommend만 존재하는 경우
+        // recommend보다 적으면 deficiency(0), 많으면 excess(1)
+        else if(userGroupNutrient.getAverageAmount()!=null&&userGroupNutrient.getRecommendAmount()!=null&&userGroupNutrient.getEnoughAmount()==null
+                &&userGroupNutrient.getMaximumAmount()==null){
+            n=2;
+        }
+        // 3. enough만 존재하는 경우
+        // enough 넘으면 많으면 excess(1)
+        else if(userGroupNutrient.getAverageAmount()==null&&userGroupNutrient.getRecommendAmount()==null&&userGroupNutrient.getEnoughAmount()!=null
+                &&userGroupNutrient.getMaximumAmount()==null){
+            n=3;
+        }
+        // 4. average, recommend, maximum만 존재하는 경우
+        // recommend보다 적으면 deficiency(0), maximum보다 많으면 excess(1)
+        else if(userGroupNutrient.getAverageAmount()!=null&&userGroupNutrient.getRecommendAmount()!=null&&userGroupNutrient.getEnoughAmount()==null
+                &&userGroupNutrient.getMaximumAmount()!=null){
+            n=4;
+        }
+        // 5. enough, maximum만 존재하는 경우
+        // maximum보다 많으면 excess(1)
+        else if(userGroupNutrient.getAverageAmount()==null&&userGroupNutrient.getRecommendAmount()==null&&userGroupNutrient.getEnoughAmount()!=null
+                &&userGroupNutrient.getMaximumAmount()!=null){
+            n=5;
+        }
+
+        switch(n) {
+            case 0:
+                throw new RuntimeException("분석 오류1");
+            case 1:
+                this.type=-1;
+            case 2:
+                // deficiency(0)
+                if(userGroupNutrient.getRecommendAmount()>nutrientAnalysisDto.getAmount()){
+                    this.type=0;
+                }
+                // excess(1)
+                else if(userGroupNutrient.getRecommendAmount()<nutrientAnalysisDto.getAmount()){
+                    this.type=1;
+                }else{
+                    this.type=-1;
+                }
+                break;
+            case 3:
+                // excess(1)
+                if(userGroupNutrient.getEnoughAmount()<nutrientAnalysisDto.getAmount()){
+                    this.type=1;
+                }else{
+                    this.type=-1;
+                }
+                break;
+            case 4:
+                if(userGroupNutrient.getRecommendAmount()>nutrientAnalysisDto.getAmount()){
+                    this.type=0;
+                }
+                // excess(1)
+                else if(userGroupNutrient.getMaximumAmount()<nutrientAnalysisDto.getAmount()){
+                    this.type=1;
+                }else{
+                    this.type=-1;
+                }
+                break;
+            case 5:
+                if(userGroupNutrient.getMaximumAmount()<nutrientAnalysisDto.getAmount()){
+                    this.type=1;
+                }else{
+                    this.type=-1;
+                }
+                break;
+            default:
+                break;
+        }
     }
 
 }
